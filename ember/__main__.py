85cf94523fa47eaf3c282a26626fc822aff466a9
Ryan Smith <smith.a.ryan.98@gmail.com>
2022-03-14 22:06:08 -0400

    add: ember compiler entry

---
+++ ember/__main__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+##-------------------------------##
+## Ember                         ##
+## Written By: Ryan Smith        ##
+##-------------------------------##
+
+## Imports
+import sys
+from pathlib import Path
+
+## Constants
+# -Errors
+ERR_USAGE: int = 64
+ERR_INPUT: int = 66
+
+## Functions
+
+## Body
+if len(sys.argv) == 1 or len(sys.argv) > 2:
+    print("No input file", file=sys.stderr)
+    sys.exit(ERR_USAGE)
+source: Path = Path(sys.argv[1])
+if not source.is_file():
+    print(f"File '{source.resolve()}' does not exist or is not a file", file=sys.stderr)
+    sys.exit(ERR_INPUT)
