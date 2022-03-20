e0348c58721bb882ab46e50d9e0914069b7ea39f
Winter_Snake <blizzardsnake98@gmail.com>
2022-03-20 15:10:07 -0400

    update(Parser): use Token class

--- ember/frontend/parser.py
+++ ember/frontend/parser.py
@@ -7,69 +7,95 @@
 ##-------------------------------##
 
 ## Imports
+import sys
 from pathlib import Path
 from typing import Any
 
+from .lexer import Token
+
 
 ## Functions
-def parse_lexemes(lexemes: list[tuple[Path, int, int, str]]) -> Any:
+def parse_tokens(tokens: list[Token]) -> list[Any] | None:
     """Return a parse tree from lexemes"""
-    lexemes: list[str] = [token[3] for token in lexemes]
-    total: int = 0
+    if not tokens:
+        return None
     nodes: list[Any] = []
-    while total < len(lexemes):
-        node, i = _parse_expression(lexemes[total:])
-        total += i
+    index: int = 0
+    while index < len(tokens):
+        node, j = _parse_expression(tokens[index:])
         nodes.append(node)
+        index += j
     return nodes
 
 
-def _parse_expression(lexemes: list[str]) -> tuple[Any, int]:
+def _parse_expression(tokens: list[Token]) -> tuple[Any, int]:
     """Return a parse tree of an expression"""
-    if lexemes[0] == "DEBUG__PRINTU__":
-        expr, i = _parse_expression_pm(lexemes[2:])  # -Handles 'DEBUG__PRINTU__' and '('
-        i += 3  # -Handles 'DEBUG__PRINTU__', '(', and ')'
+    if tokens[0].type == Token.TYPE.IDENTIFIER and tokens[0].value == "DEBUG__PRINTU__":
+        expr, index = _parse_expression_as(tokens[2:])
+        index += 3  # -Handles: 'DEBUG__PRINTU__', '(', and ')'
         expr = {"DEBUG_PRINTU": expr}
     else:
-        expr, i = _parse_expression_pm(lexemes)
-    return (expr, i + 1)  # -Handles ';'
+        expr, index = _parse_expression_as(tokens)
+    # -Handle: Unexpected end of token stream
+    if index >= len(tokens):
+        token: Token = tokens[-1]
+        print(f"{token.file_path.resolve()}:{token.row}:{token.column} Unexpected end of steam, expected ';'")
+        sys.exit(1)
+    # -Handle: Unexpected token in stream
+    elif tokens[index].type != Token.TYPE.SEMICOLON:
+        token: Token = tokens[index]
+        print(f"{token.file_path.resolve()}:{token.row}:{token.column} Unexpected token '{token}', expected ';'", file=sys.stderr)
+        sys.exit(1)
+    return (expr, index + 1)  # -Handles ';'
 
 
-def _parse_expression_pm(lexemes: list[str]) -> tuple[Any, int]:
+def _parse_expression_as(tokens: list[Token]) -> tuple[Any, int]:
     """Return a parse tree of +|- expressions"""
-    expr, i = _parse_expression_mdm(lexemes)
-    lexemes = lexemes[i:]
-    while lexemes and lexemes[0] in ('+', '-'):
-        operator: str = lexemes[0]
-        rhs, j = _parse_expression_mdm(lexemes[1:])
+    expr, index = _parse_expression_mdm(tokens)
+    while (
+        index < len(tokens) and
+        tokens[index].type in (Token.TYPE.ADD, Token.TYPE.SUB)
+    ):
+        operator: str = tokens[index].type
+        rhs, index_ = _parse_expression_mdm(tokens[index + 1:])
         expr = {operator:{'lhs': expr, 'rhs': rhs}}
-        i += (j := j + 1)
-        lexemes = lexemes[j:]
-    return (expr, i)
+        index += index_ + 1  # -Handles op and rhs
+    return (expr, index)
 
 
-def _parse_expression_mdm(lexemes: list[str]) -> tuple[Any, int]:
+def _parse_expression_mdm(tokens: list[Token]) -> tuple[Any, int]:
     """Return a parse tree of *|/|% expressions"""
-    expr, i = _parse_literal_primary(lexemes)
-    lexemes = lexemes[i:]
-    while lexemes and lexemes[0] in ('*', '/', '%'):
-        operator: str = lexemes[0]
-        rhs, j = _parse_literal_primary(lexemes[1:])
+    expr, index = _parse_literal_primary(tokens)
+    while (
+        index < len(tokens) and
+        tokens[index].type in (Token.TYPE.MUL, Token.TYPE.DIV, Token.TYPE.MOD)
+    ):
+        operator: str = tokens[index].type
+        rhs, index_ = _parse_literal_primary(tokens[index + 1:])
         expr = {operator:{'lhs': expr, 'rhs': rhs}}
-        i += (j := j + 1)
-        lexemes = lexemes[j:]
-    return (expr, i)
+        index += index_ + 1  # -Handles op and rhs
+    return (expr, index)
 
 
-def _parse_literal_primary(lexemes: list[str]) -> tuple[Any, int]:
+def _parse_literal_primary(tokens: list[Token]) -> tuple[Any, int]:
     """Return a parse tree of a primary literal"""
-    if lexemes[0] == '(':
-        expr, i = _parse_expression_pm(lexemes[1:])
-        return (expr, i + 2)  # -Handles '(' and ')'
-    return _parse_literal_number(lexemes)
+    if tokens[0].type == Token.TYPE.LPAREN:
+        expr, index = _parse_expression_as(tokens[1:])
+        # -Handle: Unexpected token in stream
+        if tokens[index + 1].type != Token.TYPE.RPAREN:
+            token: Token = tokens[index]
+            print(f"{token.file_path.resolve()}:{token.row}:{token.column} Unexpected token '{token}', expected ')'", file=sys.stderr)
+            sys.exit(1)
+        return (expr, index + 2)  # -Handles '(' and ')'
+    return _parse_literal_number(tokens)
 
 
-def _parse_literal_number(lexemes: list[str]) -> tuple[int, int]:
+def _parse_literal_number(tokens: list[Token]) -> tuple[int, int]:
     """Return a parse tree of a number literal"""
-    number = int(lexemes[0])
-    return (number, 1)
+    # -Handle: Unexpected token in stream
+    if tokens[0].type != Token.TYPE.NUMBER:
+        token: Token = tokens[0]
+        print(f"{token.file_path.resolve()}:{token.row}:{token.column} Unexpected token '{token}', expected 'NUMBER'", file=sys.stderr)
+        sys.exit(1)
+    value: int = int(tokens[0].value)
+    return (value, 1)
