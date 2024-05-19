29171ef57168201e8a2c09c6a0e0c073afc80a83
Winter_Snake <blizzardsnake98@gmail.com>
2024-05-19 05:27:33 -0400

    add: git and core files

---
+++ emberc/__main__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+##-------------------------------##
+## Ember Compiler                ##
+## Written By: Ryan Smith        ##
+##-------------------------------##
+
+## Imports
+import sys
+
+## Constants
+
+
+## Functions
+
+
+## Body
+with open("./tests/lexing.ember", 'r') as source_file:
+    source = source_file.read()
