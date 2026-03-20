f9c1375b65ed02f045d65afba9ca630cf102b62e
Winter_Snake <blizzardsnake98@gmail.com>
2026-03-20 00:19:58 -0400

    fix: headers

--- emberc/ast/unresolved/enum.py
+++ emberc/ast/unresolved/enum.py
@@ -2,7 +2,7 @@
 ## Ember Compiler                ##
 ## Written By: Ryan Smith        ##
 ##-------------------------------##
-## Unresolved Node: Variable     ##
+## Unresolved Node: Enum         ##
 ##-------------------------------##
 
 ## Imports
