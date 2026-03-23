b4eb4bc46201b85bd416e7cfe7d2a75d1a5883f4
Winter_Snake <blizzardsnake98@gmail.com>
2026-03-23 01:24:54 -0400

    add(Table): member scopes add(Table): find id by name (reverse, local, member) add(Table): enum/enum_member helpers add(Table): symbol property add(Symbol): Enum/Member + Tagged/Variant

--- emberc/middleware/symbol_table.py
+++ emberc/middleware/symbol_table.py
@@ -10,11 +10,18 @@ from __future__ import annotations
 from dataclasses import dataclass
 from enum import IntEnum, auto
 from typing import TYPE_CHECKING
-from ..ast import FunctionTypeNode
+from ..ast import (
+    PendingTypeNode,
+    FunctionTypeNode
+)
 
 if TYPE_CHECKING:
+    from collections.abc import Sequence
     from ..ast import TypeNode
     from ..core import MutableCollection
+
+## Constants
+type SCOPE = dict[str, int]
 
 
 ## Classes
@@ -29,6 +36,10 @@ class Symbol:
 
     # -Sub-Classes
     class Kind(IntEnum):
+        Enum = auto()
+        EnumMember = auto()
+        TaggedEnum = auto()
+        EnumVariant = auto()
         Function = auto()
         Parameter = auto()
         Variable = auto()
@@ -44,12 +55,14 @@ class SymbolTable:
     # -Constructor
     def __init__(self) -> None:
         self._symbols: list[Symbol] = []
-        self._scopes: list[dict[str, int]] = [{}]
+        self._scopes: list[SCOPE] = [{}]
+        self._member_scopes: dict[int, SCOPE] = {}
 
     # -Instance Methods
     def add_symbol(
         self, name: str, kind: Symbol.Kind, _type: TypeNode
     ) -> int | None:
+        '''Adds a symbol to the current scope and returns index into flat table'''
         if name in self.current_scope:
             return None
         idx = len(self._symbols)
@@ -57,7 +70,54 @@ class SymbolTable:
         self._symbols.append(Symbol(idx, name, _type, kind))
         return idx
 
+    def add_member_symbol(
+        self, parent: int, name: str, kind: Symbol.Kind, _type: TypeNode
+    ) -> int | None:
+        '''Adds a symbol to a parent's scope and returns index into flat table'''
+        if (scope := self._member_scopes.get(parent, None)) is None:
+            return None
+        if name in scope:
+            return None
+        idx = len(self._symbols)
+        scope[name] = idx
+        self._symbols.append(Symbol(idx, name, _type, kind))
+        return idx
+
+    def find_id(self, name: str) -> int | None:
+        '''Finds the given name by bubbling up through scopes'''
+        for scope in reversed(self._scopes):
+            if name in scope:
+                return scope[name]
+        return None
+
+    def find_local_id(self, name: str) -> int | None:
+        '''Finds the given name within the local scope'''
+        return self.current_scope.get(name, None)
+
+    def find_member_id(self, parent: int, name: str) -> int | None:
+        '''Find the given name within a parent's scope'''
+        if (scope := self._member_scopes.get(parent, None)) is None:
+            return None
+        return scope.get(name, None)
+
     # -Instance Methods: Helpers
+    def add_enum(
+        self, name: str, _type: TypeNode, is_tagged: bool
+    ) -> int | None:
+        kind = Symbol.Kind.TaggedEnum if is_tagged else Symbol.Kind.Enum
+        if (_id := self.add_symbol(name, kind, _type)) is None:
+            return None
+        self._member_scopes[_id] = {}
+        return _id
+
+    def add_enum_member(
+        self, parent: int, name: str, is_tagged: bool
+    ) -> int | None:
+        kind = Symbol.Kind.EnumVariant if is_tagged else Symbol.Kind.EnumMember
+        return self.add_member_symbol(
+            parent, name, kind, PendingTypeNode(parent)
+        )
+
     def add_function(
         self, name: str, return_type: TypeNode,
         parameter_types: MutableCollection[TypeNode]
@@ -74,3 +134,7 @@ class SymbolTable:
     @property
     def current_scope(self) -> dict[str, int]:
         return self._scopes[-1]
+
+    @property
+    def symbols(self) -> Sequence[Symbol]:
+        return tuple(self._symbols)
