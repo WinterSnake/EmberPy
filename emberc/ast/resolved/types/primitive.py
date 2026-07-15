2bf1a50cafc18b1b619eeb83bad440dde7e49f8a
Winter_Snake <blizzardsnake98@gmail.com>
2026-07-14 21:07:19 -0400

    add: __str__ for easy print

--- emberc/ast/resolved/types/primitive.py
+++ emberc/ast/resolved/types/primitive.py
@@ -8,7 +8,7 @@
 ## Imports
 from dataclasses import dataclass
 from enum import IntEnum, auto
-from typing import TYPE_CHECKING, ClassVar, Self
+from typing import TYPE_CHECKING, ClassVar, Self, assert_never
 from .node import TypeNode
 
 if TYPE_CHECKING:
@@ -22,6 +22,14 @@ class TypePrimitive(TypeNode):
     Resolved Primitive Type
     Represents a built-in primitive type.
     """
+    # -Dunder Methods
+    def __str__(self) -> str:
+        match self.kind:
+            case TypePrimitive.Kind.Int32:
+                return "int32"
+            case _:
+                assert_never(self.kind)
+
     # -Instance Methods
     def accept[T](self, visitor: TypeNodeVisitor[T]) -> T:
         return visitor.visit_type_primitive(self)
--- emberc/ast/unresolved/types.py
+++ emberc/ast/unresolved/types.py
@@ -8,7 +8,7 @@
 ## Imports
 from dataclasses import dataclass
 from enum import IntEnum, auto
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, assert_never
 from .node import UnresolvedNode
 
 if TYPE_CHECKING:
@@ -22,6 +22,14 @@ class UnresolvedTypeNode(UnresolvedNode):
     Unresolved Primitive Type
     Encapsulates the specific kind of built-in data type.
     """
+    # -Dunder Methods
+    def __str__(self) -> str:
+        match self.kind:
+            case UnresolvedTypeNode.Kind.Int32:
+                return "int32"
+            case _:
+                assert_never(self.kind)
+
     # -Instance Methods
     def accept[T](self, visitor: UnresolvedNodeVisitor[T]) -> T:
         return visitor.visit_type(self)
