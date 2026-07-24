3573cff06b0aee9dccfea33ba70c528c9ed363ab
Winter_Snake <blizzardsnake98@gmail.com>
2026-07-24 01:15:35 -0400

    add: push/pop stack
    refactor: next_id property getter
    docs: consistency

--- emberc/middleware/symbol_table.py
+++ emberc/middleware/symbol_table.py
@@ -22,10 +22,9 @@ type Scope = dict[str, int]
 @dataclass(slots=True)
 class Symbol:
     """
-    Represents a uniquely identified entity resolved by the compiler.
-    
-    Tracks identity, lexical identifier name, definition variant (kind), 
-    and the concrete evaluation type of a compiler primitive.
+    Ember Symbol
+
+    Foundation for name identity, including: id, symbol variant, and value type.
     """
     # -Properties
     id: int
@@ -39,60 +38,70 @@ class Symbol:
 
 
 class SymbolTable:
-    """
-    Manages lexical scopes and unique symbol assignments during compilation.
-    
-    Provides a block-structured environment for looking up variable 
-    identities, managing stacked variable scopes, and enforcing symbol isolation.
-    """
-
+    """Maps a collection of symbols to unique ids. Handles scope creation and searching."""
     # -Constructor
     def __init__(self) -> None:
-        self._symbols: list[Symbol] = []
         self._scopes: list[Scope] = [{}]
+        self._symbols: list[Symbol] = []
 
-    # -Dunder Methods
-    def __getitem__(self, index: int) -> Symbol:
-        return self._symbols[index]
-
-    # -Instance Methods
-    def add_symbol(self, name: str, kind: Symbol.Kind, _type: TypeNode) -> int | None:
-        '''
-        Creates a symbol and appends it to current scope.
-        Returns None when symbol already exists
-        '''
+    # -Instance Methods: Symbol
+    def add_symbol(
+        self, name: str, kind: Symbol.Kind, _type: TypeNode
+    ) -> int | None:
+        '''Create symbol and return id or None if symbol already exists.'''
         if name in self.current_scope:
             return None
-        idx = len(self._symbols)
-        self._symbols.append(Symbol(idx, name, kind, _type))
-        self.current_scope[name] = idx
-        return idx
+        _id = self.next_id
+        self._symbols.append(Symbol(_id, name, kind, _type))
+        self.current_scope[name] = _id
+        return _id
 
     def find_id(self, name: str) -> int | None:
-        '''Finds and returns symbol id by bubbling up scopes from top down'''
+        '''Find and return symbol id by bubbling up scopes from top-down or None if non-existent.'''
         for scope in reversed(self._scopes):
             if name in scope:
                 return scope[name]
         return None
 
-    def find_local_id(self, name: str) -> int | None:
-        '''Finds and returns symbol id for current scope or None if non-existent'''
+    def find_id_local(self, name: str) -> int | None:
+        '''Find and return symbol id within current scopet or None if non-existent.'''
         return self.current_scope.get(name, None)
+
+    # -Instance Methods: Table
+    def push(self) -> None:
+        '''Push new scope onto the stack.'''
+        self._scopes.append({})
+
+    def pop(self) -> Scope:
+        '''Pop and return scope from the stack.'''
+        return self._scopes.pop()
 
     # -Instance Methods: Helpers
     def add_variable(self, name: str, _type: TypeNode) -> int | None:
-        '''Routes variable declaration into add_symbol'''
+        '''Route and return symbol creation with variable hinting.'''
         return self.add_symbol(name, Symbol.Kind.Variable, _type)
 
     # -Properties
     @property
+    def next_id(self) -> int:
+        return len(self._symbols)
+
+    @property
     def current_scope(self) -> Scope:
+        '''Return top-level scope.'''
         return self._scopes[-1]
 
     @property
     def scope_depth(self) -> int:
+        '''Return current scope depth.'''
         return len(self._scopes) - 1
 
     @property
     def symbols(self) -> Sequence[Symbol]:
+        '''Return symbol collection from table.'''
+        assert self.scope_depth == 0, "Tried getting symbols on non-rooted table."
+        self._scopes.clear()
         return tuple(self._symbols)
+
+    # -Class Properties
+    __slots__ = ("_scopes", "_symbols")
